59b5a98f39953dfa0172673565a9df562096d7ad
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2019-12-29 20:35:55 +0200

    meteor now served from GES cleanup

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -21,22 +21,16 @@ export class GameScene extends Phaser.Scene {
   }
 
   async init(params) {
-    this.delta = 1000;
-    this.lastMeteorTime = 0;
-    this.meteorsCaught = 0;
-    this.meteorsFallen = 0;
-
     this.meteorGameData = await dataController.getExampleDataUnit();
     const endSessionElement = this.meteorGameData.gameElements.endSessionHTML.html;
-    this.add.dom(20, 600).createFromHTML(endSessionElement);
+    this.add.dom(120, 600).createFromHTML(endSessionElement);
     const sessionIdElement = this.meteorGameData.gameElements.sessionIdHTML.html;
-    this.add.dom(100, 40).createFromHTML(sessionIdElement);
+    this.add.dom(100, 20).createFromHTML(sessionIdElement);
   }
 
   async preload() {
     this.load.path = "assets";
     this.load.image("city", "/sprites/city.png");
-    this.load.image("meteor", "/sprites/meteor.png");
   }
 
   create() {
@@ -47,42 +41,10 @@ export class GameScene extends Phaser.Scene {
     Phaser.Actions.PlaceOnLine(this.city.getChildren(),
       new Phaser.Geom.Line(245, 758, 1260, 758));
     this.city.refresh();
-    this.info = this.add.text(10, 10, 'sample text',
-      { font: '24px Arial Bold', fill: '#FBFBAC' });
+    // this.info = this.add.text(10, 10, 'sample text',
+    //   { font: '24px Arial Bold', fill: '#FBFBAC' });
   }
 
   update(time: number): void {
-    var diff: number = time - this.lastMeteorTime;
-    if (diff > this.delta) {
-      this.lastMeteorTime = time;
-      this.spawnMeteor();
-    }
-    this.info.text =
-      this.meteorsCaught + " caught - " +
-      this.meteorsFallen + " fallen";
-  }
-
-  private spawnMeteor(): void {
-    // @ts-ignore reason: handwritten number
-    const x = Phaser.Math.Between(25, config.width - 25);
-    const y = 26;
-    const destroyDelay = 200;
-
-    const meteor = new Meteor(
-      this, x, y,
-      () => {
-        this.meteorsCaught += 1;
-        this.time.delayedCall(destroyDelay, (m: Meteor) => {
-          m.destroy();
-        }, [meteor], this);
-      }
-    );
-    this.physics.add.collider(meteor, this.city,
-      meteor.onFall(() => {
-        this.meteorsFallen += 1;
-        this.time.delayedCall(destroyDelay, (m: Meteor) => {
-          m.destroy();
-        }, [meteor], this);
-      }), null, this);
   }
 };
